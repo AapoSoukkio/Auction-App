47485a26cb0cabba8744fbcb973a59b98dae7914
Aapo Soukkio <aapo.soukkio@edu.savonia.fi>
2023-08-30 10:35:49 +0300

    Event buss initializing

--- src/SearchService/Program.cs
+++ src/SearchService/Program.cs
@@ -1,3 +1,4 @@
+using MassTransit;
 using Polly;
 using Polly.Extensions.Http;
 using SearchService.Data;
@@ -11,6 +12,14 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 
 builder.Services.AddHttpClient<AuctionSvcHttpClient>().AddPolicyHandler(GetPolicy());
+
+builder.Services.AddMassTransit(x =>
+{
+    x.UsingRabbitMq((context, cfg) =>
+    {
+        cfg.ConfigureEndpoints(context);
+    });
+});
 
 var app = builder.Build();
 
@@ -31,7 +40,6 @@ app.Lifetime.ApplicationStarted.Register(async () =>
         Console.WriteLine(e);
     }
 });
-
 
 app.Run();
 
