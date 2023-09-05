cdc6c32238db5c408ee993ffb5755d924e95f92f
Aapo Soukkio <aapo.soukkio@edu.savonia.fi>
2023-09-05 13:14:01 +0300

    Fetching data from the API

--- frontend/web-app/app/auctions/Listings.tsx
+++ frontend/web-app/app/auctions/Listings.tsx
@@ -1,7 +1,21 @@
 import React from 'react'
+import AuctionCard from './AuctionCard';
 
-export default function Listings() {
+async function getData() {
+    const res = await fetch('http://localhost:6001/search')
+    if (!res.ok) throw new Error('Failed to fetch data')
+
+    return res.json();
+}
+
+export default async function Listings() {
+    const data = await getData();
+    
   return (
-    <div>Listings</div>
+    <div>
+        {data && data.results.map((auction: any)=> (
+            <AuctionCard auction={auction} key={auction.id}/>
+        ))}
+    </div>
   )
 }
