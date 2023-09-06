fc24ff888a4ae9f5b809964c9c1c269943eda98e
Aapo Soukkio <aapo.soukkio@edu.savonia.fi>
2023-09-06 20:52:24 +0300

    Getting and populating the session data

--- frontend/web-app/app/nav/NavBar.tsx
+++ frontend/web-app/app/nav/NavBar.tsx
@@ -1,17 +1,25 @@
 import React from 'react'
-import {AiOutlineCar} from 'react-icons/ai'
-import Search from './Search'
-import Logo from './Logo'
-import LoginButton from './LoginButton'
+import Search from './Search';
+import Logo from './Logo';
+import LoginButton from './LoginButton';
+import { getCurrentUser } from '../actions/authActions';
+import UserActions from './UserActions';
 
-export default function NavBar() {
+export default async function Navbar() {
+  const user = await getCurrentUser();
+
   return (
     <header className='
-    sticky top-0 z-50 flex justify-between bg-white p-5 items-center text-gray-800 shadow-md
+      sticky top-0 z-50 flex justify-between bg-white p-5 items-center text-gray-800 shadow-md
     '>
       <Logo />
       <Search />
-      <LoginButton />
+      {user ? (
+        <UserActions user={user}/>
+      ) : (
+        <LoginButton />
+      )}
+
     </header>
   )
 }
