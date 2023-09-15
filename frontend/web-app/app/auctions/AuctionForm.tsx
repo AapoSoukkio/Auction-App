55b3d87669029147e7967835e66dc6872a0d4ed8
Aapo Soukkio <aapo.soukkio@edu.savonia.fi>
2023-09-15 14:35:25 +0300

    Updates to the auction form

--- frontend/web-app/app/auctions/AuctionForm.tsx
+++ frontend/web-app/app/auctions/AuctionForm.tsx
@@ -2,7 +2,7 @@
 
 import { error } from 'console';
 import { Button, TextInput } from 'flowbite-react';
-import React from 'react'
+import React, { useEffect } from 'react'
 import { FieldValues, useForm } from 'react-hook-form'
 import Input from '../components/Input';
 
@@ -11,6 +11,10 @@ export default function AuctionForm() {
         formState: { isSubmitting, isValid, isDirty, errors } } = useForm({
             mode: 'onTouched'
         });
+
+        useEffect(() => {
+            setFocus('make');
+        }, [setFocus])
 
     function onSubmit(data: FieldValues) {
         console.log(data);
@@ -22,6 +26,27 @@ export default function AuctionForm() {
                 rules={{required: 'Make is required'}}/>
             <Input label='Model' name='model' control={control}
                 rules={{required: 'Model is required'}}/>
+            <Input label='Color' name='color' control={control}
+                rules={{ required: 'Color is required' }} />
+            
+            <div className='grid grid-cols-2 gap-3'>
+                <Input label='Year' name='year' control={control} type='number'
+                    rules={{ required: 'Year is required' }} />
+                <Input label='Mileage' name='mileage' control={control} type='number'
+                    rules={{ required: 'Mileage is required' }} />
+            </div>
+
+            <Input label='Image URL' name='imageUrl' control={control}
+                rules={{ required: 'Image URL is required' }} />
+
+            <div className='grid grid-cols-2 gap-3'>
+                <Input label='Reserve Price (enter 0 if no resere)'
+                    name='reservePrice' control={control} type='number'
+                    rules={{ required: 'Reserve pirice is required' }} />
+                <Input label='Auction end date/time' name='auctionEnd'
+                    control={control} type='date'
+                    rules={{ required: 'Auction end date is required' }} />
+            </div>
          
         <div className='flex justify-between'>
             <Button outline color='gray'>Succes</Button>
