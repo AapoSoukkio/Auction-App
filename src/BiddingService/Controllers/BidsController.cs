ecba907e5bc27279d4972916e4cb0dfabc943715
Aapo Soukkio <aapo.soukkio@edu.savonia.fi>
2023-09-18 14:41:11 +0300

    Added gRPC server and gRPC client for service to service communication

--- src/BiddingService/Controllers/BidsController.cs
+++ src/BiddingService/Controllers/BidsController.cs
@@ -1,6 +1,7 @@
 ﻿using AutoMapper;
 using BiddingService.DTOs;
 using BiddingService.Models;
+using BiddingService.Services;
 using Contracts;
 using MassTransit;
 using Microsoft.AspNetCore.Authorization;
@@ -15,12 +16,15 @@ namespace BiddingService.Controllers
     {
         private readonly IMapper _mapper;
         private readonly IPublishEndpoint _publishEndpoint;
+        private readonly GrpcAuctionClient _grpcClient;
+
         //private readonly GrpcAuctionClient _grpcClient;
 
-        public BidsController(IMapper mapper, IPublishEndpoint publishEndpoint)
+        public BidsController(IMapper mapper, IPublishEndpoint publishEndpoint, GrpcAuctionClient grpcClient)
         {
             _mapper = mapper;
             _publishEndpoint = publishEndpoint;
+            _grpcClient = grpcClient;
         }
 
         [Authorize]
@@ -31,6 +35,8 @@ namespace BiddingService.Controllers
 
             if (auction == null)
             {
+                auction = _grpcClient.GetAuction(auctionId);
+
                 if (auction == null) return BadRequest("Cannot accept bids on this auction at this time");
             }
 
